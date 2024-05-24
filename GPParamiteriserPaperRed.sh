0fffbcac72db99b978345ed9fd2a4689a21b8cd6
peter clark <57264084+npc101-ncl@users.noreply.github.com>
2024-05-24 14:03:53 +0100

    nearly missed this file

---
+++ GPParamiteriserPaperRed.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Example SLURM job script for serial (non-parallel) jobs
+#
+#
+# Tell SLURM if you want to be emailed when your job starts, ends, etc.
+# Currently mail can only be sent to addresses @ncl.ac.uk
+#
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=peter.clark@ncl.ac.uk
+#SBATCH --mem-per-cpu=5G
+#
+
+python GPParamiteriser.py slurm dataName:redPaper id:zr75 twoStage:yes antStr:antStringRedM.txt preAntStr:antStringRedF.txt parallelAntStr:antStringRedMP.txt stabAlow:200.0 methP:method:particle_swarm,swarm_size:100,iteration_limit:5000 scaleRefs:PEScaleRefs_zr75.csv paramOut:parameters_zr75.csv scaleOut:scales_zr75.csv TCOutputs:PETimeCourses_zr75.csv DBOutputs:DBTimeCourses_zr75.csv
