458709c8141168862794735446b54eb44ed855bd
peter clark <57264084+npc101-ncl@users.noreply.github.com>
2024-05-29 11:53:22 +0100

    removing newcastle email
    the script files were set to email my old newcastle email adress every time they ran.

--- GPParamiteriserToy.sh
+++ GPParamiteriserToy.sh
@@ -2,11 +2,6 @@
 # Example SLURM job script for serial (non-parallel) jobs
 #
 #
-# Tell SLURM if you want to be emailed when your job starts, ends, etc.
-# Currently mail can only be sent to addresses @ncl.ac.uk
-#
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=peter.clark@ncl.ac.uk
 #SBATCH --mem-per-cpu=5G
 #
 
